1419ec597c6f2e7496f410bec1588720f822c7ae
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-03 15:38:03 +0900

    bird 기본 구조 만들기

--- Bird.py
+++ Bird.py
@@ -1,2 +1,23 @@
-import pico2d
+from pico2d import load_image, get_time, load_font
+import game_world
+import game_framework
+from state_machine import StateMachine
+
+class Bird:
+    image = None
+
+    def __init__(self):
+        self.x = 0
+        self.y = 500
+        self.image = load_image('bird_animation.png')
+        pass
+
+    def draw(self):
+        pass
+
+    def do(self):
+        pass
+
+    def update(self):
+        pass
 
