b5cf2d61f27d52ac56d55881e126097010acbcc6
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-03 15:39:47 +0900

    랜덤값으로 위치 랜덤설정

--- Bird.py
+++ Bird.py
@@ -1,4 +1,5 @@
 from pico2d import load_image, get_time, load_font
+import random
 import game_world
 import game_framework
 from state_machine import StateMachine
@@ -7,8 +8,9 @@ class Bird:
     image = None
 
     def __init__(self):
-        self.x = 0
-        self.y = 500
+        self.x = random.randint(100, 500)
+        self.y = random.randint(300, 500)
+        self.dir = 1
         self.image = load_image('bird_animation.png')
         pass
 
