07a4dd83015c170d1a848a71f84f1598629ece8a
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-03 15:44:01 +0900

    비행속도같은 변수 추가. 수업에서 한것에서 따옴, 변수이름변경

--- Bird.py
+++ Bird.py
@@ -4,6 +4,17 @@ import game_world
 import game_framework
 from state_machine import StateMachine
 
+
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+FLY_SPEED_KMPH = 20.0 # Km / Hour
+FLY_SPEED_MPM = (FLY_SPEED_KMPH * 1000.0 / 60.0)
+FLY_SPEED_MPS = (FLY_SPEED_MPM / 60.0)
+FLY_SPEED_PPS = (FLY_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class Bird:
     image = None
 
